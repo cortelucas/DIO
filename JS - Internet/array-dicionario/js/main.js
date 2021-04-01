a7778ffacca9e1cd87ddf28e1b5ec1e77511089f
Lucas <corte1994.lc@gmail.com>
2021-04-01 10:05:25 -0300

    - arrays e dicionarios

---
+++ JS - Internet/array-dicionario/js/main.js
@@ -0,0 +1,31 @@
+let frutas = [
+    {
+        nome: "maçã",
+        cor: "vermelho"
+    },
+    {
+        nome: "pêra",
+        cor: "amarelo esverdeado"
+    },
+    {
+        nome: "laranja",
+        cor: "laranja ou verde"
+    },
+    {
+        nome: "uva",
+        cor: "roxo"
+    },
+];
+
+frutas.forEach((fruta) => {
+    console.log(`A ${fruta.nome} é da cor ${fruta.cor}`);
+});
+
+// let frutas = ['maçã', 'pêra', 'laranja'];
+// frutas.push('uva');
+
+// console.log(frutas.join('\n'));
+
+// frutas.forEach((fruta) => {
+//     console.log(fruta);
+// });
