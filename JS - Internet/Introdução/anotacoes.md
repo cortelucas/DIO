aa0e706ec1ebc14607aa6022854351a788606412
Lucas <corte1994.lc@gmail.com>
2021-04-01 09:50:50 -0300

    - introdução

---
+++ JS - Internet/Introdução/anotacoes.md
@@ -0,0 +1,5 @@
+#Introdução
+
+---
+
+DOM - modelo de árvore
---
+++ JS - Internet/Introdução/js/main.js
@@ -0,0 +1,8 @@
+let nome = 'Lucas S Corte';
+let idade = 27;
+
+//alert(`Olá ${nome}, você tem ${idade} anos!`); // = alert('Olá ' + nome + ', você tem ' + idade + ' anos!');
+
+console.log(`Olá ${nome}, você tem ${idade} anos!`);
+console.log(nome.toLowerCase());
+console.log(nome.toUpperCase());
