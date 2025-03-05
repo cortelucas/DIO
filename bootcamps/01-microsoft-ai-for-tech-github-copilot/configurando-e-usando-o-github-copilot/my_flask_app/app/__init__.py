4599080700d56c0c4fc97d64070dcd4105226f6a
cortelucas <corte1994.lc@gmail.com>
2025-03-05 10:34:57 -0300

    refactor: colocando no padrão pep

--- bootcamps/01-microsoft-ai-for-tech-github-copilot/configurando-e-usando-o-github-copilot/my_flask_app/app/__init__.py
+++ bootcamps/01-microsoft-ai-for-tech-github-copilot/configurando-e-usando-o-github-copilot/my_flask_app/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
+
 def create_app():
-    app = Flask(__name__, template_folder='../templates')
+    app = Flask(__name__, template_folder="../templates")
 
     from . import routes
+
     app.register_blueprint(routes.bp)
 
     return app
--- bootcamps/01-microsoft-ai-for-tech-github-copilot/configurando-e-usando-o-github-copilot/my_flask_app/app/routes.py
+++ bootcamps/01-microsoft-ai-for-tech-github-copilot/configurando-e-usando-o-github-copilot/my_flask_app/app/routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template
 
-bp = Blueprint('main', __name__)
+bp = Blueprint("main", __name__)
 
-@bp.route('/')
+
+@bp.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
