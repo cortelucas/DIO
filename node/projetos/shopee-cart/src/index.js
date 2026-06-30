5e12ba74fb0245025154acae2853ef6912a519e7
cortelucas <corte1994.lc@gmail.com>
2026-06-30 12:31:55 -0300

    feat: Implemented the logic for adding an item to the cart and calculating the total.

--- node/projetos/shopee-cart/src/index.js
+++ node/projetos/shopee-cart/src/index.js
@@ -1,9 +1,11 @@
+import { Cart } from "./services/cart.js";
 import { Item } from "./services/item.js";
 
 const cart = [];
 
 console.log("Welcome to the your Shopee Cart");
 
+const cartInstance = new Cart();
 const item = new Item();
 
 const hotwheelsFerrari = await item.createItem("Hotwheels Ferrari", 20.99, 1);
@@ -13,13 +15,6 @@ const hotwheelsLamborghini = await item.createItem(
 	3,
 );
 
-console.log([
-	{
-		...hotwheelsFerrari,
-		subtotal: hotwheelsFerrari.subtotal(),
-	},
-	{
-		...hotwheelsLamborghini,
-		subtotal: hotwheelsLamborghini.subtotal(),
-	},
-]);
+await cartInstance.addItem(cart, hotwheelsFerrari);
+await cartInstance.addItem(cart, hotwheelsLamborghini);
+console.log(`Your cart total is: ${await cartInstance.calculateTotal(cart)}`);
--- node/projetos/shopee-cart/src/services/cart.js
+++ node/projetos/shopee-cart/src/services/cart.js
@@ -1,9 +1,15 @@
 export class Cart {
-	async addItem(userCart) {}
+	async addItem(userCart, item) {
+		userCart.push(item);
+		return userCart;
+	}
 
 	async removeItem(userCart, index) {}
 
 	async deleteItem(userCart, name) {}
 
-	async calculateTotal(userCart) {}
+	async calculateTotal(userCart) {
+		const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
+		return `R$ ${result.toFixed(2)}`;
+	}
 }
