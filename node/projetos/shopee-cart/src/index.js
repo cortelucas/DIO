eec0edcae0a8bbaf2c5e4c165fe56f8c665e514f
cortelucas <corte1994.lc@gmail.com>
2026-06-30 12:41:23 -0300

    feat: add items to whishlist

--- node/projetos/shopee-cart/src/index.js
+++ node/projetos/shopee-cart/src/index.js
@@ -1,20 +1,33 @@
 import { Cart } from "./services/cart.js";
 import { Item } from "./services/item.js";
 
-const cart = [];
+const myCart = [];
+const myWhishlist = [];
 
 console.log("Welcome to the your Shopee Cart");
 
 const cartInstance = new Cart();
 const item = new Item();
 
-const hotwheelsFerrari = await item.createItem("Hotwheels Ferrari", 20.99, 1);
+const hotwheelsFerrari = await item.createItem("Hotwheels Ferrari", 20.99, 2);
 const hotwheelsLamborghini = await item.createItem(
 	"Hotwheels Lamborghini",
 	39.99,
 	3,
 );
 
-await cartInstance.addItem(cart, hotwheelsFerrari);
-await cartInstance.addItem(cart, hotwheelsLamborghini);
-console.log(`Your cart total is: ${await cartInstance.calculateTotal(cart)}`);
+await cartInstance.addItem(myCart, hotwheelsFerrari);
+console.log(`Your cart total is: ${await cartInstance.calculateTotal(myCart)}`);
+
+await cartInstance.addItem(myWhishlist, hotwheelsLamborghini);
+await cartInstance.addItem(myWhishlist, await item.createItem("Hotwheels Porsche", 21.99, 4));
+
+console.log(`Your whishlist contens: ${myWhishlist.length} items`);
+console.log(myWhishlist.map((item) => {
+    return {
+        name: item.name,
+        price: item.price,
+        quantity: item.quantity,
+        subtotal: item.subtotal()
+    };
+}));
